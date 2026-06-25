714a232288ab832e4faeedabaea632f29a6651b1
Harsh-Prajapati1491 <102405824+Harsh-Prajapati1491@users.noreply.github.com>
2026-06-25 23:44:54 +0530

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -euo pipefail
 
+mkdir -p /logs/verifier
+
+pytest /tests/test_outputs.py --json-ctrf=/logs/verifier/ctrf.json -rA
+
+if [ $? -eq 0 ]; then
+  echo "1" > /logs/verifier/reward.txt
+else
+  echo "0" > /logs/verifier/reward.txt
+fi
